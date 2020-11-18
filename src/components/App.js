2ee2958d8dce0e5a7cdd29c8dd418a6a3dd4acf7
Tassia Accioly <tassia.accioly@gmail.com>
2020-11-18 15:49:58 -0300

    done with bugs

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
 
 import './App.css';
-import countriesSrc from '../countries.json';
-import HomePage from './homepage/HomePage';
-import Navbar from './Navbar';
 import CountriesList from './countrieslist/CountriesList';
 import CountryDetail from './countrydetails/CountryDetails';
 
 class App extends React.Component {
   state = {
-    countries: [...countriesSrc],
+    countries: [],
+    loading: true,
   };
+
+  async componentDidMount() {
+    try {
+      //isso vai conter o objeto de resposta do HTTP, por isso o response
+      const response = await axios.get(
+        'https://countries.tech-savvy.tech/countries'
+      );
+
+      console.log(response);
+
+      this.setState({ countries: [...response], loading: false });
+    } catch (err) {
+      //para saber em que linha está o erro
+      console.log(err);
+    }
+  }
 
   render() {
     return (
       <div className="App">
         <BrowserRouter>
-          <nav class="navbar navbar-dark bg-primary mb-3">
-            <div class="container">
-              <a class="navbar-brand" href="/">
+          <nav className="navbar navbar-dark bg-primary mb-3">
+            <div className="container">
+              <a className="navbar-brand" href="/">
                 WikiCountries
               </a>
             </div>
@@ -31,7 +46,13 @@ class App extends React.Component {
                 className="col-5"
                 style={{ maxHeight: '90vh', overflow: 'scroll' }}
               >
-                <CountriesList countries={this.state.countries} />
+                {this.state.loading ? (
+                  <div className="spinner-grow text-info" role="status">
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  <CountriesList countries={this.state.countries} />
+                )}
               </div>
               <div className="col-7">
                 <Route
